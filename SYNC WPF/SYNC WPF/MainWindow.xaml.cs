c0f4be27f9e0cffb4d42bc0d7b57203c16e50541
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-05-17 02:22:44 +0300

    Final SYNC WPF

--- SYNC WPF/SYNC WPF/MainWindow.xaml.cs
+++ SYNC WPF/SYNC WPF/MainWindow.xaml.cs
@@ -20,9 +20,93 @@ namespace SYNC_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Window1 son;
         public MainWindow()
         {
             InitializeComponent();
+            color1.Background = Brushes.Red;
+            
+            color2.Background = Brushes.Red;
+            color3.Background = Brushes.Red;
+            color4.Background = Brushes.Red;
+            color5.Background = Brushes.Red;
+            color6.Background = Brushes.Red;
+            son = new Window1(this);
+        }
+
+        private void choose_Color(Brush current, int index)
+        {
+            index--;
+            if (current == Brushes.Green)
+            {
+                son.Greens[index].IsChecked = true;
+            }
+            if (current == Brushes.Blue)
+            {
+                son.Blues[index].IsChecked = true;
+            }
+            if (current == Brushes.Red)
+            {
+                son.Reds[index].IsChecked = true;
+            }
+        }
+        private void show_click(object sender, RoutedEventArgs e)
+        {
+            son.Show();
+            choose_Color(color1.Background, 1);
+            choose_Color(color2.Background, 2);
+            choose_Color(color3.Background, 3);
+            choose_Color(color4.Background, 4);
+            choose_Color(color5.Background, 5);
+            choose_Color(color6.Background, 6);
+        }
+
+        private Brush next_Color(int index, Brush current)
+        {
+            --index;
+            if (current == Brushes.Red)
+            {
+                if (son.IsVisible)
+                    son.Greens[index].IsChecked = true;
+                return Brushes.Green;
+            }
+            else if (current == Brushes.Green)
+            {
+                if (son.IsVisible)
+                    son.Blues[index].IsChecked = true;
+                return Brushes.Blue;
+            }
+            else if (current == Brushes.Blue)
+            {
+                if (son.IsVisible)
+                    son.Reds[index].IsChecked = true;
+                return Brushes.Red;
+            }
+            return Brushes.Black;
+        }
+        private void color1_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            color1.Background = next_Color(1, color1.Background);
+        }
+        private void color2_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            color2.Background = next_Color(2, color2.Background);
+        }
+        private void color3_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            color3.Background = next_Color(3, color3.Background);
+        }
+        private void color4_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            color4.Background = next_Color(4, color4.Background);
+        }
+        private void color5_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            color5.Background = next_Color(5, color5.Background);
+        }
+        private void color6_MouseLeftButtonUp(object sender, EventArgs e)
+        {
+            color6.Background = next_Color(6, color6.Background);
         }
     }
 }
---
+++ SYNC WPF/SYNC WPF/Window1.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SYNC_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        MainWindow main;
+
+        public RadioButton[] Reds;
+        public RadioButton[] Greens;
+        public RadioButton[] Blues;
+        public Window1(MainWindow _main)
+        {
+            InitializeComponent();
+            main = _main;
+            Reds = new RadioButton[6];
+            Reds[0] = Red1; Reds[1] = Red2; Reds[2] = Red3; Reds[3] = Red4; Reds[4] = Red5; Reds[5] = Red6;
+            Greens = new RadioButton[6];
+            Greens[0] = Green1; Greens[1] = Green2; Greens[2] = Green3; Greens[3] = Green4; Greens[4] = Green5; Greens[5] = Green6;
+            Blues = new RadioButton[6];
+            Blues[0] = Blue1; Blues[1] = Blue2; Blues[2] = Blue3; Blues[3] = Blue4; Blues[4] = Blue5; Blues[5] = Blue6;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        private Brush CheckButton(object sender)
+        {
+            if ((sender as RadioButton).Name[0] == 'R')
+                return Brushes.Red;
+            else if ((sender as RadioButton).Name[0] == 'G')
+                return Brushes.Green;
+            else if ((sender as RadioButton).Name[0] == 'B')
+                return Brushes.Blue;
+            return Brushes.Black;
+        }
+
+        private void red1_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)!(sender as RadioButton).IsChecked || main == null)
+                return;
+            main.color1.Background = CheckButton(sender);
+        }
+        private void red2_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)!(sender as RadioButton).IsChecked || main == null )
+                return;
+            main.color2.Background = CheckButton(sender);
+        }
+        private void red3_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)!(sender as RadioButton).IsChecked || main == null)
+                return;
+            main.color3.Background = CheckButton(sender);
+        }
+        private void red4_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)!(sender as RadioButton).IsChecked || main == null)
+                return;
+            main.color4.Background = CheckButton(sender);
+        }
+        private void red5_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)!(sender as RadioButton).IsChecked || main == null)
+                return;
+            main.color5.Background = CheckButton(sender);
+        }
+        private void red6_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)!(sender as RadioButton).IsChecked || main == null)
+                return;
+            main.color6.Background = CheckButton(sender);
+        }
+    }
+}
